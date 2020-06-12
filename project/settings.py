87bd22473a3e9fe7d2595438c21e60be21af0322
unknown <werttrew788@gmail.com>
2020-06-12 13:09:03 +0200

    make it better

--- project/settings.py
+++ project/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '!1h@m0df9bt**2tw6dg%e&%-bt&qq0i@v6#vx-789574jdwbd%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['wert88.herokuapp.com', '127.0.0.1']
 
