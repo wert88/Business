873155edb6b01d6abc963aa35fc2052fdff3967b
unknown <werttrew788@gmail.com>
2020-06-12 13:16:16 +0200

    make it better

--- project/urls.py
+++ project/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
