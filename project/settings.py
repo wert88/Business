c4186f76fef86fa568dbc136ac83d0440d61a1da
unknown <werttrew788@gmail.com>
2020-06-12 12:47:56 +0200

    make it better

--- project/settings.py
+++ project/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = '!1h@m0df9bt**2tw6dg%e&%-bt&qq0i@v6#vx-789574jdwbd%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['wert88.herokuapp.com']
 
 
 # Application definition
